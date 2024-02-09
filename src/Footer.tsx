59cb238ba6f94fc6c96aed4b14aa9022c3e9c031
Arenswald Stephan <stephan.arenswald@mercedes-benz.com>
2024-02-09 18:50:09 +0100

    fix: footer line break on small devices missing

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -7,20 +7,22 @@ function Footer (): ReactElement {
 
   return (
     <footer className='text-xs text-gray-500 py-4 absolute bottom-0 mt-0 z-10 text-center flex flex-col gap-3 bg-gray-100 w-full'>
-      <div className='flex flex-row gap-2 justify-center'>
-        <span><span className='line-through italic text-gray-400'>Invented </span>Written down by: </span>
-        <a href='https://twitter.com/sarensw'>
-          <img src={X} className='w-4' />
-        </a>
-        <a href='https://linkedin.com/in/stephanarenswald'>
-          <img src={LinkedIn} className='w-4' />
-        </a>
-        <span>Stephan Arenswald</span>
+      <div className='flex md:flex-row flex-col gap-2 justify-center'>
+        <span><span className='line-through italic text-gray-400'>Invented</span> Written down by: </span>
+        <div className='flex flex-row gap-2 justify-center'>
+          <a href='https://twitter.com/sarensw'>
+            <img src={X} className='w-4' />
+          </a>
+          <a href='https://linkedin.com/in/stephanarenswald'>
+            <img src={LinkedIn} className='w-4' />
+          </a>
+          <span>Stephan Arenswald</span>
 
-        <a href='https://linkedin.com/in/joachimandreasritter'>
-          <img src={LinkedIn} className='w-4 ml-4' />
-        </a>
-        <span>Joachim Ritter</span>
+          <a href='https://linkedin.com/in/joachimandreasritter'>
+            <img src={LinkedIn} className='w-4 ml-4' />
+          </a>
+          <span>Joachim Ritter</span>
+        </div>
       </div>
       <p>
         &copy; {year} SarensX OÜ <a className='ml-8' href='/imprint.html'>Impressum</a>
