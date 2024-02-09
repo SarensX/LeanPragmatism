d4416c651b538f232304b59ea191e7d1abbb31b9
Arenswald Stephan <stephan.arenswald@mercedes-benz.com>
2024-02-09 18:41:40 +0100

    fix: imprint not loaded

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'imprint.html'),
+      },
+    },
+  },
   base: '/',
   plugins: [react()],
 })
